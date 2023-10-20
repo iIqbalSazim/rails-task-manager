fb2be71ca88d8fac42dd718d308fee67969a2d6b
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-10-20 14:09:20 +0600

    Set up edit routes and update task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
   end
 
   def new
@@ -10,8 +15,33 @@ class TasksController < ApplicationController
   def create
     current = params[:task]
     @task = Task.new({"title": "#{current[:title]}", "body": "#{current[:body]}"})
+
     if @task.save
       redirect_to tasks_path, notice: "Successfully added new task"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+  end
+
+  def update
+    edited = params[:task]
+    if @task.update({"title": "#{edited[:title]}", "body": "#{edited[:body]}"})
+      redirect_to tasks_path, notice: "Task was successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_tweet
+    @task = Task.find(params[:id])
+  end
+
 end
